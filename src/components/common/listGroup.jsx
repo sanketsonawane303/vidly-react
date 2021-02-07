ae532e90d55e1a72746b94dc546bf61028583dfc
sanketsonawane303 <62289047+sanketsonawane303@users.noreply.github.com>
2021-02-07 17:39:05 +0530

    added filtering with listGroup

---
+++ src/components/common/listGroup.jsx
@@ -0,0 +1,36 @@
+const ListGroup = (props) => {
+  const {
+    items,
+    textProperty,
+    valueProperty,
+    selectedItem,
+    onItemSelect,
+  } = props;
+
+  return (
+    <ul className="list-group">
+      {items.map((item) => (
+        // eslint-disable-next-line jsx-a11y/anchor-is-valid
+        <a
+          key={item[valueProperty]}
+          className={
+            item === selectedItem
+              ? 'list-group-item list-group-item-action active'
+              : 'list-group-item list-group-item-action'
+          }
+          style={{ cursor: 'pointer' }}
+          onClick={() => onItemSelect(item)}
+        >
+          {item[textProperty]}
+        </a>
+      ))}
+    </ul>
+  );
+};
+
+ListGroup.defaultProps = {
+  textProperty: 'name',
+  valueProperty: '_id',
+};
+
+export default ListGroup;

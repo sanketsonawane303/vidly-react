40dc3ac8355ba39bbecd2bcc6aa2a209d30e1a62
sanketsonawane303 <62289047+sanketsonawane303@users.noreply.github.com>
2021-02-11 21:58:25 +0530

    removed no movies in db message

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -95,11 +95,8 @@ class Movies extends Component {
   }
 
   render() {
-    const { length: count } = this.state.movies;
     const { currentPage, pageSize, sortColumn, searchQuery } = this.state;
     const { user } = this.props;
-
-    if (count === 0) return <p>There are no movies in the database.</p>;
 
     const { totalCount, data: movies } = this.getPagedData();
 
